28ae4b6e338c8756c518324312c643e6d4f0d2cf
Dandioktada <64734599+Dandioktada@users.noreply.github.com>
2020-05-09 21:37:01 +0800

    mengunggah file hasil belajar php dasar pertemuan3

---
+++ Latihan PHP Dasar/pertemuan3/latihan2.php
@@ -0,0 +1,19 @@
+<?php 
+for ($i=0; $i <9 ; $i++) { 
+	if ($i%2 ) {
+		echo "$i ganjil <br>";
+	} else{
+		echo "$i genap <br>";
+	}
+}
+echo "<hr>";
+$x =5;
+if ($x < 20) {
+	echo "Benar";
+} else if (condition) {
+	echo "Bingo!";
+} else {
+	echo "Salah";
+}
+
+ ?>
